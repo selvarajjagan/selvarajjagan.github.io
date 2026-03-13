059f84d9f0f08b1eab8a514e208a620bc3cad9f5
Selvaraj Jaganathan <jselvaraj.js@gmail.com>
2026-03-13 20:13:06 +0530

    Fetch and display top 6 repos sorted by stars

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 const username = "selvarajjagan";
 
 fetch(`https://api.github.com/users/${username}/repos`)
-.then(res => res.json())
+.then(response => response.json())
 .then(data => {
 
 const container = document.getElementById("repo-container");
 
-data.slice(0,6).forEach(repo => {
+data
+.sort((a,b)=>b.stargazers_count-a.stargazers_count)
+.slice(0,6)
+.forEach(repo => {
 
 const card = document.createElement("div");
 
-card.className="card";
+card.className = "card";
 
 card.innerHTML = `
 <h3>${repo.name}</h3>
-<p>${repo.description || "No description"}</p>
-<a href="${repo.html_url}" target="_blank">View Project</a>
+<p>${repo.description || "No description available"}</p>
+<a href="${repo.html_url}" target="_blank">View Repository</a>
 `;
 
 container.appendChild(card);
